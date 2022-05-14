f36cdc563306d7550f9627d08405f76a3423f2c5
DNayd <93384678+DNayd@users.noreply.github.com>
2022-05-14 21:00:52 +0300

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -4,4 +4,4 @@ seqs = input('Введите имя .fasta файла: ')
 sequence = SeqIO.parse(open(seqs), 'fasta')
 print(f"{'Name':<10} {'Length':<10} {'Bases frequency:':<50}")
 for feature in sequence:
-    print(f'{feature.name:<5} {len(feature):<5} {str(dict(Counter(feature))):<50}')
+    print(f'{feature.name:<10} {len(feature):<10} {str(dict(Counter(feature))):<50}')
