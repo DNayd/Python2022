f4dfdc690da716156d37ee3de6f994742b8820d2
DNayd <93384678+DNayd@users.noreply.github.com>
2022-05-14 20:59:44 +0300

    Добавлена возможность ввода любого файла

--- Task 2.py
+++ Task 2.py
@@ -1,6 +1,7 @@
 from Bio import SeqIO
 from collections import Counter
-sequence = SeqIO.parse(open('/Users/dnayd/Documents/Python/pythonProject2022/sequences.fasta'), 'fasta')
+seqs = input('Введите имя .fasta файла: ')
+sequence = SeqIO.parse(open(seqs), 'fasta')
 print(f"{'Name':<10} {'Length':<10} {'Bases frequency:':<50}")
 for feature in sequence:
-    print(f'{feature.name:<10} {len(feature):<10} {str(dict(Counter(feature))):<50}')
+    print(f'{feature.name:<5} {len(feature):<5} {str(dict(Counter(feature))):<50}')
